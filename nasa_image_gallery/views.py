9130aa3ab60a1acd85f5cbe9d397fb9c1cee7356
Anabella <anabellasantillan023@gmail.com>
2024-06-05 10:15:53 -0300

    views.py y services_nasa_image_gallery.py funcionales. buscador funcional

--- nasa_image_gallery/views.py
+++ nasa_image_gallery/views.py
@@ -12,27 +12,29 @@ def index_page(request):
 
 # auxiliar: retorna 2 listados -> uno de las imágenes de la API y otro de los favoritos del usuario.
 def getAllImagesAndFavouriteList(request):
-    images = []
-    favourite_list = []
+    images = services_nasa_image_gallery.getAllImages()
+    #favourite_list = []
 
-    return images, favourite_list
+    return images #, #favourite_list
 
 # función principal de la galería.
 def home(request):
-    # llama a la función auxiliar getAllImagesAndFavouriteList() y obtiene 2 listados: uno de las imágenes de la API y otro de favoritos por usuario*.
-    # (*) este último, solo si se desarrolló el opcional de favoritos; caso contrario, será un listado vacío [].
-    images = []
-    favourite_list = []
-    return render(request, 'home.html', {'images': images, 'favourite_list': favourite_list} )
+    # llama a la función auxiliar getAllImagesAndFavouriteList() y obtiene 2 listados: uno de las imágenes de la API y otro de favoritos por usuario.
+    # () este último, solo si se desarrolló el opcional de favoritos; caso contrario, será un listado vacío [].
+    images = getAllImagesAndFavouriteList(request)
+    #favourite_list = []
+    return render(request, 'home.html', {'images': images} )
 
-
-# función utilizada en el buscador.
 def search(request):
-    images, favourite_list = getAllImagesAndFavouriteList(request)
+    images = getAllImagesAndFavouriteList(request) # images, favourite_list = getAllImagesAndFavouriteList(request)
     search_msg = request.POST.get('query', '')
-
+    if search_msg == "":
+        return render (request, 'home.html', {'images':images})
+    else:
+        filtro = services_nasa_image_gallery.getImagesBySearchInputLike(search_msg)
+        return render (request, 'home.html', { 'images' : filtro})
     # si el usuario no ingresó texto alguno, debe refrescar la página; caso contrario, debe filtrar aquellas imágenes que posean el texto de búsqueda.
-    pass
+
 
 
 # las siguientes funciones se utilizan para implementar la sección de favoritos: traer los favoritos de un usuario, guardarlos, eliminarlos y desloguearse de la app.
